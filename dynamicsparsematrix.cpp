46c0f2564ec0b28a5e0d64fdee1a7e1ef8197143
ArtNlk <artemthegood@gmail.com>
2021-12-23 16:51:03 +0300

    Implemented DynamicSparseMatrix

--- dynamicsparsematrix.cpp
+++ dynamicsparsematrix.cpp
@@ -1,1 +1,55 @@
 #include "dynamicsparsematrix.h"
+
+DynamicSparseMatrix::DynamicSparseMatrix(int size, int avgRowLength) :
+    m_size(size),
+    m_rows(size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        m_rows[i].reserve(avgRowLength);
+    }
+}
+
+void DynamicSparseMatrix::resize(int newSize)
+{
+    m_size = newSize;
+    m_rows.resize(newSize);
+}
+
+int DynamicSparseMatrix::size() const
+{
+    return m_size;
+}
+
+void DynamicSparseMatrix::setValue(int rowIndex, int columnIndex, double value)
+{
+    SparseRow &targetRow = m_rows[rowIndex];
+    for(int column = 0; column < m_size; column++)
+    {
+        if(targetRow[column].first == columnIndex)
+        {
+            targetRow[column].second = value;
+            return;
+        }
+        else if(targetRow[column].first > columnIndex)
+        {
+            targetRow.insert(targetRow.begin()+column,SparseRowUnit(column,value));
+            return;
+        }
+    }
+    targetRow.push_back(SparseRowUnit(columnIndex,value));
+}
+
+double DynamicSparseMatrix::getValue(int rowIndex, int columnIndex) const
+{
+    const SparseRow &targetRow = m_rows[rowIndex];
+    for(int column = 0; column < m_size; column++)
+    {
+        if(targetRow[column].first == columnIndex)
+        {
+            return targetRow[column].second;
+        }
+    }
+
+    return 0;
+}
--- dynamicsparsematrix.h
+++ dynamicsparsematrix.h
@@ -7,13 +7,21 @@
 class DynamicSparseMatrix
 {
 public:
-    DynamicSparseMatrix();
+    typedef std::pair<int,double> SparseRowUnit;
+    typedef std::vector<SparseRowUnit> SparseRow;
+
+    DynamicSparseMatrix(int size, int avgRowLength = 7);
+
+
+    void resize(int newSize);
+    int size() const;
 
     void setValue(int rowIndex, int columnIndex, double value);
-    double getValue(int rowIndex, int columnIndex);
+    double getValue(int rowIndex, int columnIndex) const;
 
 protected:
-    std::vector<std::vector<std::pair<int,double>>> m_rows;
+    std::vector<SparseRow> m_rows;
+    int m_size;
 };
 
 #endif // DYNAMICSPARSEMATRIX_H
